20dc80eb7dd9975e86b9f09b82bf8d5ea79b4525
demesheo <d.wilsonii91@gmail.com>
2015-07-14 11:18:40 -0700

    refreshing chats list with unique messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,6 +6,7 @@ var app = {
   init : function(){
 
     this.roomNames = {};
+    this.messages = [];
 
     $('a.username').click(function(){
       app.addFriend();
@@ -13,19 +14,18 @@ var app = {
 
     $( "#send .submit" ).submit(app.handleSubmit);
     
-    app.fetch();
-
     $('#roomSelect').change(function(e){
       var selectedRoom = "." + e.target.value;
 
       $('#chats div').show(selectedRoom);
       $("#chats div").not(selectedRoom).hide();
 
-      // if('#chats:not('+selectedRoom+')'){
-      //   $('#chats:not('+selectedRoom+')').toggle();
-      // }
-      //if ($('select option:selected')!== $('div:'))
     });
+
+    $('.spinner').hide();
+
+    app.fetch();
+    setInterval(app.fetch.bind(this), 5000);
 
     return true;
 
@@ -43,101 +43,84 @@ var app = {
       error: function (data) {
           // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
           console.error('chatterbox: Failed to send message');
-      },
+        },
 
 
-    }
+      }
     )},
+
     fetch : function(){ 
+
+      $('.spinner').show();
       var that = this;
-      // $.ajax({
-      //   url: 'https://api.parse.com/1/classes/chatterbox',
-      //   type: 'GET',
-      // //data: data,
-      //   success: function( data ) {
-      //     //( ".result" ).html( data );
-      //     console.log(data);  
-      //     for(var i = 0; i < data.results.length; i++){
-      //       if(data.results[i].roomname===undefined){
-      //         var roomname = "undefined";
-      //       } else if (data.results[i].roomname===null){
-      //         var roomname = "null";
-      //       }else if (data.results[i].roomname==="" || data.results[i].roomname===" "){
-      //         var roomname = "_";
-      //       }else {var roomname = data.results[i].roomname;}
-      //       that.roomNames[roomname] = roomname;
-      //       that.addMessage(data.results[i]);
-      //     }
 
-      //     //
-      //     console.table(data.result);
-      //     for(var key in that.roomNames){
-      //       var addedRoom = that.roomNames[key];
-      //       that.addRoom(addedRoom);
-      //     }
-      //   },
-      //   dataType: "jsonp"
-      // });
+      $.get('https://api.parse.com/1/classes/chatterbox', function( data ) {
 
-   $.get('https://api.parse.com/1/classes/chatterbox', function( data ) {
-   //( ".result" ).html( data );
-   console.log(data);  
-   for(var i = 0; i < data.results.length; i++){
-     if(data.results[i].roomname===undefined){
-       var roomname = "undefined";
-     } else if (data.results[i].roomname===null){
-       var roomname = "null";
-     }else if (data.results[i].roomname==="" || data.results[i].roomname===" "){
-       var roomname = "_";
-     }else {var roomname = data.results[i].roomname;}
-     that.roomNames[roomname] = roomname;
-     that.addMessage(data.results[i]);
-   }
+        $('.spinner').hide();
+        that.clearMessages();
 
-   //
-   console.table(data.result);
-   for(var key in that.roomNames){
-     var addedRoom = that.roomNames[key];
-     that.addRoom(addedRoom);
-   }
+        that.messages = that.messages.concat(data.results);
+        that.messages = _.uniq(that.messages, function(item){
+        return item.objectId;
+      })
 
+      that.messages = _.sortBy(that.messages, function(item){
+        return new Date(item.createdAt).getTime();
+      }).reverse();
 
+      console.table(that.messages);
 
+      for(var i = 0; i < that.messages.length; i++){
+        if(that.messages[i].roomname===undefined){
+          var roomname = "undefined";
+        } else if (that.messages[i].roomname===null){
+          var roomname = "null";
+        }else if (that.messages[i].roomname==="" || that.messages[i].roomname===" "){
+          var roomname = "_";
+        }else {
+          var roomname = that.messages[i].roomname;
+        }
+        that.roomNames[roomname] = roomname;
+        that.addMessage(that.messages[i]);
+      }
 
+      //
+      console.table(data.result);
+      for(var key in that.roomNames){
+        var addedRoom = that.roomNames[key];
+        that.addRoom(addedRoom);
+      }
+    })
+  },
 
+  clearMessages : function(){$('#chats').html('');},
 
-  })
-},
+  addMessage : function(message){
 
-clearMessages : function(){$('#chats').html('');},
+    if(typeof message.text !== 'string' || message.text === undefined || message.username === undefined || message.text.indexOf('<script>') !== -1 || message.username.indexOf('<script>') !== -1){
+      var $message = $('<div class="'+ message.roomname +'"><a class="username">THIS USER IS UNTRUSTED</a>: THIS MESSAGE IS UNTRUSTED <br>'+message.createdAt+'</div>');
+      $('#chats').append($message);
+    }else{
+      var $message = $('<div class="' + message.roomname + '"><a class="username">'+ message.username +'</a>'+ ": "+message.text +' <br>'+message.createdAt+'</div>');
+      $('#chats').append($message);
+    }
 
-addMessage : function(message){
-  console.log(message);
-  if(typeof message.text !== 'string' || message.text === undefined || message.username === undefined || message.text.indexOf('<script>') !== -1 || message.username.indexOf('<script>') !== -1){
-    var $message = $('<div class="'+ message.roomname +'"><a class="username">THIS USER IS UNTRUSTED</a>: THIS MESSAGE IS UNTRUSTED</div>');
-    $('#chats').append($message);
-  }else{
-    var $message = $('<div class="' + message.roomname + '"><a class="username">'+ message.username +'</a>'+ ": "+message.text +'</div>');
-    $('#chats').append($message);
+  },
+
+  addRoom : function(roomname){
+    var $newRoom = $('<option class="room" value="'+roomname+'">'+roomname+'</option>');
+    $('#roomSelect').append($newRoom);
+  },
+
+  addFriend : function(){},
+
+  handleSubmit : function(){ 
+    console.log('handling submit');
+    //return true;
   }
-},
-
-addRoom : function(roomname){
-  var $newRoom = $('<option class="room" value="'+roomname+'">'+roomname+'</option>');
-  $('#roomSelect').append($newRoom);
-},
-
-addFriend : function(){
-},
-
-handleSubmit : function(){ 
-  console.log('handling submit');
-  //return true;
-}
-
 
 };
 
 $( document ).ready(function() {
-    app.init();
+  app.init();
 });
